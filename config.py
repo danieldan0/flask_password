38ee7090a666633adcfc8ea9fb75bb7ff1fa0298
Daniel N <daniel.nakushnov@gmail.com>
2025-11-30 23:59:50 +0200

    Added reCAPTCHA

--- config.py
+++ config.py
@@ -4,3 +4,5 @@ class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///app.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    RECAPTCHA_PUBLIC_KEY = os.environ.get("RECAPTCHA_PUBLIC_KEY") or "your-public-key"
+    RECAPTCHA_PRIVATE_KEY = os.environ.get("RECAPTCHA_PRIVATE_KEY") or "your-private-key"
--- forms.py
+++ forms.py
@@ -1,14 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
 from wtforms.validators import DataRequired, Email
-# from flask_wtf.recaptcha import RecaptchaField
+from flask_wtf.recaptcha import RecaptchaField
 from util.security import validate_strong_password
 
 class RegisterForm(FlaskForm):
     email = StringField("Email", validators=[Email()])
     password = PasswordField("Password", validators=[DataRequired(), validate_strong_password])
     confirm_password = PasswordField("Confirm Password", validators=[DataRequired()])
-    # recaptcha = RecaptchaField()
+    recaptcha = RecaptchaField()
 
 class LoginForm(FlaskForm):
     email = StringField("Email", validators=[Email()])
